c52cdaedfdc3182b5f32a1bc2673ff459c589f2d
nerim <nerimartinez@gmail.com>
2023-05-10 12:26:50 +0200

    Airtable edit dialog

--- connect_transformations/static/transformations/airtable_lookup.c8fe185b19d9d4c56651.js
+++ connect_transformations/static/transformations/airtable_lookup.c8fe185b19d9d4c56651.js
@@ -170,6 +170,10 @@ All rights reserved.
 
 
 
+const cleanCopyRows = parent => {
+  parent.innerHTML = '';
+};
+
 const createCopyRow = (parent, index, options, input, output) => {
   const item = document.createElement('div');
   item.classList.add('list-wrapper');
@@ -220,13 +224,16 @@ const createOptions = (selectId, options) => {
 };
 
 const removeDisabled = selector => document.getElementById(selector).removeAttribute('disabled');
+const cleanField = elem => {
+  elem.setAttribute('disabled', '');
+  elem.value = '';
+};
 
 const airtable = (app) => {
   if (!app) return;
   hideComponent('loader');
   showComponent('app');
 
-  let columns = [];
   let airtableColumns = [];
   let apiKey;
   let baseId;
@@ -234,20 +241,30 @@ const airtable = (app) => {
   let tables;
   let mapInputColumn;
   let mapAirtableColumn;
+  const baseSelect = document.getElementById('base-select');
+  const content = document.getElementById('content');
+  const tableSelect = document.getElementById('table-select');
+  const keyInput = document.getElementById('key-input');
   const inputColumnSelect = document.getElementById('input-column-select');
   const airtableFieldSelect = document.getElementById('field-select');
   const addButton = document.getElementById('add');
 
-  app.listen('config', (config) => {
-    const baseSelect = document.getElementById('base-select');
-    const content = document.getElementById('content');
-    const tableSelect = document.getElementById('table-select');
-    const keyInput = document.getElementById('key-input');
+  app.listen('config', async (config) => {
+    const {
+      context: { available_columns: availableColumns },
+      columns: { output: outputColumns },
+      settings,
+    } = config;
+
     let airtableBases;
     let rowIndex = 0;
-    columns = config.context.available_columns;
 
     keyInput.addEventListener('input', async () => {
+      cleanField(baseSelect);
+      cleanField(tableSelect);
+      cleanField(inputColumnSelect);
+      cleanField(airtableFieldSelect);
+      cleanCopyRows(content);
       apiKey = keyInput.value;
       if (apiKey.length < 50) return;
 
@@ -266,6 +283,11 @@ const airtable = (app) => {
     });
 
     baseSelect.addEventListener('change', async () => {
+      cleanField(tableSelect);
+      cleanField(inputColumnSelect);
+      cleanField(airtableFieldSelect);
+      cleanCopyRows(content);
+
       baseId = baseSelect.value;
       tables = await getAirtableTables(apiKey, baseId);
       hideError();
@@ -275,19 +297,22 @@ const airtable = (app) => {
     });
 
     tableSelect.addEventListener('change', () => {
+      cleanField(inputColumnSelect);
+      cleanField(airtableFieldSelect);
+      cleanCopyRows(content);
       tableId = tableSelect.value;
       const currentTable = tables.find(x => x.id === tableId);
       airtableColumns = currentTable.columns;
       hideError();
 
       createOptions('field-select', airtableColumns);
-      createOptions('input-column-select', columns);
+      createOptions('input-column-select', availableColumns);
       removeDisabled('field-select');
       removeDisabled('input-column-select');
     });
 
     inputColumnSelect.addEventListener('change', () => {
-      mapInputColumn = columns.find((column) => column.id === inputColumnSelect.value);
+      mapInputColumn = availableColumns.find((column) => column.id === inputColumnSelect.value);
       if (mapAirtableColumn) removeDisabled('add');
       hideError();
     });
@@ -302,6 +327,59 @@ const airtable = (app) => {
       rowIndex += 1;
       createCopyRow(content, rowIndex, airtableColumns);
     });
+
+    if (settings) {
+      showComponent('loader');
+      apiKey = settings.api_key;
+      baseId = settings.base_id;
+      tableId = settings.table_id;
+
+      try {
+        airtableBases = await getAirtableBases(apiKey);
+        tables = await getAirtableTables(apiKey, settings.base_id);
+
+        if (airtableBases.error) {
+          throw new Error(airtableBases.error);
+        }
+        hideError();
+      } catch (e) {
+        showError(e);
+      }
+
+      const currentTable = tables.find(x => x.id === settings.table_id);
+      airtableColumns = currentTable.columns;
+
+      createOptions('base-select', airtableBases);
+      createOptions('table-select', tables);
+      createOptions('field-select', airtableColumns);
+      createOptions('input-column-select', availableColumns);
+
+      keyInput.value = settings.api_key;
+      baseSelect.value = settings.base_id;
+      tableSelect.value = settings.table_id;
+
+      mapInputColumn = availableColumns
+        .find((column) => column.name === settings.map_by.input_column);
+      inputColumnSelect.value = mapInputColumn.id;
+
+      mapAirtableColumn = airtableColumns
+        .find((column) => column.name === settings.map_by.airtable_column);
+      airtableFieldSelect.value = mapAirtableColumn.id;
+
+      removeDisabled('base-select');
+      removeDisabled('table-select');
+      removeDisabled('field-select');
+      removeDisabled('input-column-select');
+      removeDisabled('add');
+
+      settings.mapping.forEach((mapping, i) => {
+        const inputColumn = airtableColumns.find((column) => column.name === mapping.from);
+        const outputColumn = outputColumns.find((column) => column.name === mapping.to);
+        rowIndex = i;
+        createCopyRow(content, rowIndex, airtableColumns, inputColumn, outputColumn);
+      });
+      hideComponent('loader');
+    }
   });
 
   app.listen('save', async () => {
