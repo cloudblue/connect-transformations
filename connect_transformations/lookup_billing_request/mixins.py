965097ba008a4df1036af5fe54ea1d1e1207ea66
Aleksandra Ovchinnikova <nsanjka@gmail.com>
2023-11-14 22:27:50 -0800

    lite-29028 Change retrieve billing request logic

--- connect_transformations/lookup_billing_request/mixins.py
+++ connect_transformations/lookup_billing_request/mixins.py
@@ -5,7 +5,7 @@
 #
 from typing import Dict
 
-from connect.client import AsyncConnectClient, ClientError
+from connect.client import AsyncConnectClient
 from connect.eaas.core.decorators import router, transformation
 from connect.eaas.core.inject.asynchronous import get_installation_client
 from connect.eaas.core.responses import RowTransformationResponse
@@ -52,7 +52,7 @@ class LookupBillingRequestTransformationMixin:
                 self.billing_settings['asset_column']
             ]
 
-        lookup['asset.params.name'] = self.billing_settings['parameter']['name']
+        lookup['asset.params.id'] = self.billing_settings['parameter']['name']
         lookup['asset.params.value'] = value
 
         try:
@@ -69,9 +69,7 @@ class LookupBillingRequestTransformationMixin:
 
     async def extract_row_from_billing(self, request, output_columns, item_id):
         row = {}
-
         item_attrs = []
-        asset = None
 
         for col_name, col_config in output_columns.items():
             value = None
@@ -83,8 +81,6 @@ class LookupBillingRequestTransformationMixin:
                         value = param_data['value']
                         break
             elif attr.startswith('items.'):
-                if attr == 'items.old_quantity':
-                    asset = await self.get_asset(request)
                 item_attr = attr.split('.')[-1]
                 item_attrs.append((col_name, item_attr))
                 value = ''
@@ -94,26 +90,19 @@ class LookupBillingRequestTransformationMixin:
             row[col_name] = value
 
         if item_attrs:
-            self.extract_item_attrs_from_billing(item_attrs, row, request, item_id, asset)
+            self.extract_items_from_billing(item_attrs, row, request, item_id)
 
         return row
 
-    def extract_item_attrs_from_billing(  # noqa: CCR001
-        self,
-        item_attrs,
-        row,
-        request,
-        item_id_value,
-        asset,
-    ):
+    def extract_items_from_billing(self, item_attrs, row, request, item_id_value):  # noqa: CCR001
         item_id = self.billing_settings['item']['id']
         for item in request['items']:
             if item_id == 'all' or item.get(item_id) == item_id_value:
                 for col_name, item_attr in item_attrs:
-                    if item_attr == 'old_quantity' and asset:
-                        items = [x for x in asset.get('items') if x['id'] == item['id']]
-                        asset_item = items[0] if item else {}
-                        item_value = str(asset_item.get(item_attr, ''))
+                    if item_attr == 'old_quantity':
+                        items = [x for x in request['asset']['items'] if x['id'] == item['id']]
+                        old_item = items[0] if item else {}
+                        item_value = str(old_item.get(item_attr, ''))
                     else:
                         item_value = str(item.get(item_attr, ''))
                     row[col_name] += f'{SEPARATOR}{item_value}' if row[col_name] else item_value
@@ -136,61 +125,51 @@ class LookupBillingRequestTransformationMixin:
         batch_context = self.transformation_request['batch']['context']
         period_end = batch_context.get('period', {}).get('end')
         additional_filters = {}
+        additional_ff_filters = {}
         if period_end:
             additional_filters['events.created.at__lt'] = period_end
+            additional_ff_filters['updated__lt'] = period_end
 
-        requests = self.installation_client('subscriptions').requests.filter(
+        # get latest approved FF request for given lookup
+        ff_req = await self.installation_client.requests.filter(
+            status='approved',
+            **lookup,
+            **additional_ff_filters,
+        ).select(
+            '-activation_key',
+            '-template',
+        ).order_by('-updated').first()
+        if not ff_req:
+            if self.billing_settings.get('action_if_not_found') == 'fail':
+                raise BillingRequestLookupError(f'No result found for the filter {lookup}')
+            return
+
+        # get first billing request after FF request
+        additional_filters['events.created.at__gt'] = ff_req['updated']
+        billing_request = await self.installation_client('subscriptions').requests.filter(
             **lookup,
             **additional_filters,
-        ).order_by('-events.created.at')
-        requests = [r async for r in requests]
+        ).order_by('events.created.at').first()
+        if not billing_request:
+            if self.billing_settings.get('action_if_not_found') == 'fail':
+                raise BillingRequestLookupError(f'No result found for the filter {lookup}')
+            return
 
-        result = None
+        # check that there are no approved request between
+        additional_ff_filters['updated__gt'] = ff_req['updated']
+        additional_ff_filters['updated__lt'] = billing_request['events']['created']['at']
+        ff_req_count = await self.installation_client.requests.filter(
+            status='approved',
+            **additional_ff_filters,
+        ).count()
+        if ff_req_count > 1:
+            if self.billing_settings.get('action_if_not_found') == 'fail':
+                raise BillingRequestLookupError(f'No result found for the filter {lookup}')
+            return
 
-        for item in requests:
-            if result is None:
-                result = item
-            elif self.billing_settings.get('action_if_multiple') == 'leave_empty':
-                return
-            elif self.billing_settings.get('action_if_multiple') == 'use_most_actual':
-                return result
-            else:
-                raise BillingRequestLookupError(f'Many results found for the filter {lookup}')
-
-        if result:
-            return result
-
-        if self.billing_settings.get('action_if_not_found') == 'fail':
-            raise BillingRequestLookupError(f'No result found for the filter {lookup}')
-
-    async def get_asset(self, billing_request):
-        k = 'billing-asset-'
-        k += f'{billing_request["asset"]["id"]}-{billing_request["events"]["created"]["at"]}'
-        try:
-            return self.cache_get(k)
-        except KeyError:
-            pass
-
-        result = await self.retrieve_asset_from_ff(billing_request)
-
-        await self.acache_put(k, result)
-        return result
-
-    async def retrieve_asset_from_ff(self, billing_request):
-        try:
-            request = await self.installation_client.requests.filter(
-                asset__id=billing_request['asset']['id'],
-                status='approved',
-                updated__lt=billing_request['events']['created']['at'],
-            ).select(
-                '-activation_key',
-                '-template',
-            ).order_by('-updated').first()
-        except ClientError:
-            return None
-
-        if request:
-            return request['asset']
+        billing_request['asset']['params'] = ff_req['asset']['params']
+        billing_request['asset']['items'] = ff_req['asset']['items']
+        return billing_request
 
     @property
     def billing_settings(self):
